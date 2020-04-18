8fc864acf6d1d697851fc0ee99ce8d0e0825d585
John <demay.10@osu.edu>
2020-04-18 15:33:49 -0400

    modified gitignore

---
+++ config/index.py
@@ -0,0 +1,7 @@
+class DatabaseConfig():
+
+    def __init__(self):
+        self.host = "laxstats.c1rvoewfjdxl.us-east-2.rds.amazonaws.com"
+        self.user = "laxadmin"
+        self.passwd = "laxstats#32"
+        self.database = "laxstats"
