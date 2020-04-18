6544733c97f19bf7962cbba331214da41a23a13e
John <demay.10@osu.edu>
2020-04-18 15:45:40 -0400

    modified repo structure

--- config/index.py
+++
@@ -1,7 +0,0 @@
-class DatabaseConfig():
-
-    def __init__(self):
-        self.host = "laxstats.c1rvoewfjdxl.us-east-2.rds.amazonaws.com"
-        self.user = "laxadmin"
-        self.passwd = "laxstats#32"
-        self.database = "laxstats"
